abc0384818f1d71b875a6ea9fe21dd752b4efa20
Dekun Ma <william@dekun.me>
2022-05-30 13:21:32 -0400

    Init with coperception lib

--- utils/models/base/IntermediateModelBase.py
+++
@@ -1,9 +0,0 @@
-from utils.models.backbone.Backbone import *
-from utils.models.base.DetModelBase import DetModelBase
-
-
-class IntermediateModelBase(DetModelBase):
-    def __init__(self, config, layer=3, in_channels=13, kd_flag=True, layer1_channel=64, layer2_channel=128, layer3_channel=256):
-        super().__init__(config, layer, in_channels, kd_flag)
-        self.u_encoder = LidarEncoder(height_feat_size=in_channels, layer1_channel=layer1_channel, layer2_channel=layer2_channel, layer3_channel=layer3_channel)
-        self.decoder = LidarDecoder(height_feat_size=in_channels, layer1_channel=layer1_channel, layer2_channel=layer2_channel, layer3_channel=layer3_channel)
